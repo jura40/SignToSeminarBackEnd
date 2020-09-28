adab01086b442c6ca39e16dfb7f2f08b2ae740b9
Jura <djura.dzakula@gmail.com>
2020-09-28 17:27:01 +0200

    Add project files.

---
+++ SignToSeminarBackEnd/Controllers/SeminarsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SignToSeminarBackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeminarsController : ControllerBase
+    {
+        // GET: api/Seminars
+        [HttpGet]
+        public IEnumerable<Seminar> Get()
+        {
+            using (var contaxt = new ApplicationDbContext())
+            {
+                var seminars = contaxt.Seminars.ToArray();
+                return seminars;
+            }
+        }
+
+        // GET: api/Seminars/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Seminars
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Seminars/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
